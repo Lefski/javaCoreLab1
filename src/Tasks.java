33bc21b8bf95198fd4efacf9b820aa848cc6102d
Lefski <popolpopolov@yandex.ru>
2022-10-26 22:36:01 +0300

    tasks 3, 4 done

---
+++ src/Tasks.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Tasks {
+
+
+    void doTask3(){
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int sumOfArray = 0;
+        for (int j : array) {
+            sumOfArray += j;
+        }
+        System.out.println("Сумма чисел в массиве: " + sumOfArray);
+        System.out.println("Ср арифметическое чисел в массиве: " + sumOfArray / array.length);
+    }
+
+    void doTask4(){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int number = -1;
+        int maxNumber = -2_147_483_647;
+        int minNumber = 2_147_483_647;
+        System.out.println("Вводите числа в массив. Число 0 будет считаться концом воода.");
+        while (number != 0){
+            number = scanner.nextInt();
+            list.add(number);
+            if (number > maxNumber) {
+                maxNumber = number;
+            }
+            if (number < minNumber && number!=0){
+                minNumber = number;
+            }
+        }
+        System.out.println("Максимальный элемент: " + maxNumber + " Минимальный элемент: " + minNumber);
+
+    }
+}
